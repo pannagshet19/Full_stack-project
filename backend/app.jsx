1ff75b1813ec97cb251dc92b2d00c5740b5ff000
pannagshet19 <pannagshet7@gmail.com>
2025-04-04 03:10:45 +0530

    Update app.jsx
    database adding

--- backend/app.jsx
+++ backend/app.jsx
@@ -1,12 +1,24 @@
-const express= require("express");
-const app=express();
+import mongoose from 'mongoose'
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
 
-app.use("/",(req,res,next) =>{
+
+//const express= require("express");
+const app = express();
+
+
+mongoose.connect(`mongodb+srv://pannagshet7:${process.env.MONGODB_PASSWORD}@cluster0.qzlv7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => app.listen(6000, () => console.log("connected to database and server"))).catch((e) => console.log(e));
+
+app.use("/", (req, res, next) => {
     res.send("hI good morning");
 });
 
 //server 
-app.listen(6000,()=>{
-    console.log(`connected to local host port${7000}`);
-    
+app.listen(6000, () => {
+    console.log(`connected to local host port${6000}`);
+
 });
+
+
+//password SgV9zOrsddc3LiLm 
